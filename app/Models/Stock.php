51db488aaf91a0de6d24c7a05076dfdce28723df
Danichou1994 <para.sogo@ism.edu.sn>
2026-07-06 14:07:43 +0200

    Fix Stock model and controller

---
+++ app/Models/Stock.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Stock extends Model
+{
+    protected $fillable = [
+        'article_id',
+        'type',
+        'quantite',
+        'prix_unitaire',
+        'vente_id',
+        'reference',
+        'motif',
+        'stock_avant',
+        'stock_apres',
+        'date_mouvement'
+    ];
+
+    protected $casts = [
+        'date_mouvement' => 'date',
+    ];
+
+    public function article(): BelongsTo
+    {
+        return $this->belongsTo(Article::class);
+    }
+
+    public function vente(): BelongsTo
+    {
+        return $this->belongsTo(Vente::class);
+    }
+}
