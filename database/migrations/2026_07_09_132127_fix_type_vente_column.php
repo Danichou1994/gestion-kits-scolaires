d1de335d6196be780396e8cfe7e9e06ada91d2b2
Danichou1994 <para.sogo@ism.edu.sn>
2026-07-09 15:25:11 +0200

    Fix: remove problematic migration and use new one

--- database/migrations/2026_07_09_132127_fix_type_vente_column.php
+++ database/migrations/2026_07_09_132127_fix_type_vente_column.php
@@ -8,27 +8,20 @@ return new class extends Migration
 {
     public function up()
     {
+        // Sur PostgreSQL, on supprime la contrainte et on modifie le type
         $driver = DB::getDriverName();
         
         if ($driver === 'pgsql') {
-            // PostgreSQL
+            DB::statement("ALTER TABLE ventes DROP CONSTRAINT IF EXISTS ventes_type_vente_check");
             DB::statement("ALTER TABLE ventes ALTER COLUMN type_vente TYPE text");
             DB::statement("ALTER TABLE ventes ALTER COLUMN type_vente SET DEFAULT 'kit'");
         } else {
-            // MySQL
             DB::statement("ALTER TABLE ventes MODIFY type_vente ENUM('article', 'kit', 'mixte') NOT NULL DEFAULT 'kit'");
         }
     }
 
     public function down()
     {
-        $driver = DB::getDriverName();
-        
-        if ($driver === 'pgsql') {
-            DB::statement("ALTER TABLE ventes ALTER COLUMN type_vente TYPE text");
-            DB::statement("ALTER TABLE ventes ALTER COLUMN type_vente SET DEFAULT 'kit'");
-        } else {
-            DB::statement("ALTER TABLE ventes MODIFY type_vente ENUM('article', 'kit') NOT NULL DEFAULT 'kit'");
-        }
+        // Rien
     }
 };
